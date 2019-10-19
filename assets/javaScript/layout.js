d7d7d614dc4eef97574e4dd181ede5e0907ec0b3
Saleall <gebi9@hotmail.com>
2019-10-19 22:15:34 +0200

    awesome side navbar

--- assets/javaScript/layout.js
+++ assets/javaScript/layout.js
@@ -1,27 +1,6 @@
 // layout control
-function startTimer(duration, display) {
-    var timer = duration,
-        minutes, seconds;
-    setInterval(function() {
-        minutes = parseInt(timer / 60, 10);
-        seconds = parseInt(timer % 60, 10);
-
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
-
-        display.textContent = minutes + ":" + seconds;
-
-        if (--timer < 0) {
-            timer = duration;
-        }
-    }, 1000);
-}
 
 // start with page
 window.onload = function() {
     console.log('////test: started!..');
-
-    var fiveMinutes = 60 * 5,
-        display = document.querySelector('#time');
-    startTimer(fiveMinutes, display);
 };
